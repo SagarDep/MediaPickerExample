806bc247ae50024ad44785bf1767d62c914d176a
prashantm <prashatm@vinfotech.com>
2016-10-12 14:37:24 +0530

    Added more features in library.

--- app/src/main/java/smartandrodev/te/MainActivity.java
+++ app/src/main/java/smartandrodev/te/MainActivity.java
@@ -2,6 +2,7 @@ package smartandrodev.te;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import multiplemediapicker.CallBackMediaPicker;
 import multiplemediapicker.MediaPicker;
@@ -17,18 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        instance.setHeaderBackgroundColor(R.color.colorPrimary);
         // below functions to customize media picker
       /*  instance.setNumberOfColoums(2);
-        instance.setHeaderBackgroundColor(R.color.colorPrimary);
+
         instance.setHandleColor(R.color.colorPrimary);
         instance.setMaximumSelectionLimit(15);
-        instance.setHeaderTitleColor(R.color.white);*/
+        instance.setHeaderTitleColor(R.color.white);
+        instance.setSingleSelectionOn(true);*/
+    }
+
+    public void imagesPick(View v) {
 
         instance.PickMedia(this, true, new CallBackMediaPicker() {
             @Override
             public void onClick(String[] paths, boolean isImage) {
-                for (String xxx : paths) {
-                    System.out.println("PATH" + xxx);
+                for (String path : paths) {
+                    System.out.println("image PATH : " + path);
+                }
+            }
+        });
+    }
+
+    public void videoPick(View v) {
+        instance.PickMedia(this, false, new CallBackMediaPicker() {
+            @Override
+            public void onClick(String[] paths, boolean isImage) {
+                for (String path : paths) {
+                    System.out.println("Video PATH : " + path);
                 }
             }
         });
